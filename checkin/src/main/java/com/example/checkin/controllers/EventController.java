a8b1280040374f04eee54d38728ac28059734387
Pormiiles <andrelima.santos2370@gmail.com>
2024-04-10 18:15:05 -0300

    Modificando retorno do método getEvent

--- checkin/src/main/java/com/example/checkin/controllers/EventController.java
+++ checkin/src/main/java/com/example/checkin/controllers/EventController.java
@@ -3,9 +3,15 @@ package com.example.checkin.controllers;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import com.example.checkin.dto.event.EventIdDTO;
+import com.example.checkin.dto.event.EventRequestDTO;
+import com.example.checkin.dto.event.EventResponseDTO;
 import com.example.checkin.services.EventService;
 
 import lombok.RequiredArgsConstructor;
@@ -17,9 +23,18 @@ public class EventController {
     private final EventService eventService;
 
     @GetMapping("/{id}") // Path Variable - recebe o valor que o usuário forneceu
-    public ResponseEntity<String> getEvent(@PathVariable String id) {
-        this.eventService.getEventDetails(id);
+    public ResponseEntity<EventResponseDTO> getEvent(@PathVariable String id) {
+        EventResponseDTO event = this.eventService.getEventDetails(id);
 
-        return ResponseEntity.ok("Sucesso!");
+        return ResponseEntity.ok(event);
+    }
+
+    @PostMapping
+    public ResponseEntity<EventIdDTO> createEvent(@RequestBody EventRequestDTO body, UriComponentsBuilder uri) {
+        EventIdDTO eventIdDTO = this.eventService.createEvent(body);
+
+        var uri2 = UriComponentsBuilder.fromPath("/events/{id}").buildAndExpand(eventIdDTO.eventId()).toUri();
+
+        return ResponseEntity.created(uri2).body(eventIdDTO);
     }
 }
