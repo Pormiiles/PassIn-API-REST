27411434e9925caf7bb962f5ba80969ce7152387
Pormiiles <andrelima.santos2370@gmail.com>
2024-04-10 17:36:17 -0300

    Criando DTO EventResponse

---
+++ checkin/src/main/java/com/example/checkin/dto/event/EventResponseDTO.java
@@ -0,0 +1,11 @@
+package com.example.checkin.dto.event;
+
+import com.example.checkin.domain.event.Event;
+
+public class EventResponseDTO { 
+    EventDetailDTO event;
+
+    public EventResponseDTO(Event event, Integer numberOfAttendees) {
+        this.event = new EventDetailDTO(event.getId(), event.getTitle(), event.getDetails(), event.getSlug(), event.getMaximumAttendees(), numberOfAttendees);
+    }
+}
