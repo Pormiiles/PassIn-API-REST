6cc54a8ac446a59e5103b7b1d11f2cca5d410da0
Pormiiles <andrelima.santos2370@gmail.com>
2024-04-10 16:54:11 -0300

    Modificando método getEvent

--- checkin/src/main/java/com/example/checkin/controllers/EventController.java
+++ checkin/src/main/java/com/example/checkin/controllers/EventController.java
@@ -2,14 +2,24 @@ package com.example.checkin.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.checkin.services.EventService;
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/events")
+@RequiredArgsConstructor
 public class EventController {
-    @GetMapping
-    public ResponseEntity<String> getTest() {
+    private final EventService eventService;
+
+    @GetMapping("/{id}") // Path Variable - recebe o valor que o usuário forneceu
+    public ResponseEntity<String> getEvent(@PathVariable String id) {
+        this.eventService.getEventDetails(id);
+
         return ResponseEntity.ok("Sucesso!");
     }
 }
