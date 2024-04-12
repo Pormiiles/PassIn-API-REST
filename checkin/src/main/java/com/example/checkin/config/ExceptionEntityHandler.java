0066b39dd5fddf18beb3ffc7719bfec212676520
Pormiiles <andrelima.santos2370@gmail.com>
2024-04-12 20:23:15 -0300

    Tratando exceções no ExceptionEntityHandler

--- checkin/src/main/java/com/example/checkin/config/ExceptionEntityHandler.java
+++ checkin/src/main/java/com/example/checkin/config/ExceptionEntityHandler.java
@@ -1,15 +1,35 @@
 package com.example.checkin.config;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.example.checkin.domain.attendee.exceptions.AttendeeAlreadyExistsException;
+import com.example.checkin.domain.attendee.exceptions.AttendeeNotFoundException;
+import com.example.checkin.domain.event.exceptions.EventFullException;
 import com.example.checkin.domain.event.exceptions.EventNotFoundException;
+import com.example.checkin.dto.general.ErrorResponseDTO;
 
 @ControllerAdvice
 public class ExceptionEntityHandler {
     @ExceptionHandler(EventNotFoundException.class)
     public ResponseEntity handleEventNotFound(EventNotFoundException exception) {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(EventFullException.class)
+    public ResponseEntity<ErrorResponseDTO> handleEventFull(EventFullException exception) {
+        return ResponseEntity.badRequest().body(new ErrorResponseDTO(exception.getMessage()));
+    }
+
+    @ExceptionHandler(AttendeeNotFoundException.class)
+    public ResponseEntity handleAttendeeNotFound(AttendeeNotFoundException exception) {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(AttendeeAlreadyExistsException.class)
+    public ResponseEntity handleAttendeeAlreadyExists(AttendeeAlreadyExistsException exception) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).build();
     }
 }
---
+++ checkin/src/main/java/com/example/checkin/dto/general/ErrorResponseDTO.java
@@ -0,0 +1,5 @@
+package com.example.checkin.dto.general;
+
+public record ErrorResponseDTO(String message) {
+    
+}
